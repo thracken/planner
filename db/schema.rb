d59df852cd9c253501d46efdce06d10411635522
thracken <jeffgreenland.1@gmail.com>
2016-07-28 20:06:49 -0700

    Added admin value to User model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160727211425) do
+ActiveRecord::Schema.define(version: 20160729025013) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160727211425) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
