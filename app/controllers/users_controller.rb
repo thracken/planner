a1403400980e463c2efc1bbe9f97de297b714bca
thracken <jeffgreenland.1@gmail.com>
2016-07-28 20:20:36 -0700

    Took out admin check from correct_user (for now).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -56,6 +56,6 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless(current_user?(@user) || @user.admin?)
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
