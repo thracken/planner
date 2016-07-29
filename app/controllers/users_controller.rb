9805e81eec30d09cf15236a21738793c2fe5900c
thracken <jeffgreenland.1@gmail.com>
2016-07-28 19:32:26 -0700

    Added ability to edit User info.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  end
+
   private
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
