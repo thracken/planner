c15e9e8f316f88a4f987b3b9d6f898d5f1007112
Jeff <jeffgreenland.1@gmail.com>
2016-08-04 11:06:07 -0700

    Added an actual homepage with placholder text.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'users#index'
+  root 'static_pages#home'
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
